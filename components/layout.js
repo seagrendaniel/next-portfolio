bd7a0903c96a36362c7c6292e56f671c20f3e934
blackdanamite <seagrendaniel562@gmail.com>
2020-04-20 15:16:44 -0700

    add styling

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,52 @@
 import Link from 'next/link';
 
 export default ({ children, title }) => (
-  <div>
-    <header> 
-      <Link href="/"><a>Home</a></Link>  
-      <Link href="/about"><a>About</a></Link>  
-      <Link href="/hireme"><a>Hire Me</a></Link>  
+  <div className="root">
+    <header>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/about"><a>About</a></Link>
+      <Link href="/hireme"><a>Hire Me</a></Link>
     </header>
 
     <h1>{title}</h1>
     {children}
 
-<footer>&copy; {new Date().getFullYear()}</footer>
+    <footer>&copy; {new Date().getFullYear()}</footer>
+
+    <style jsx>{`
+      .root{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      }
+      header{
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+        padding: 1em;
+        font-size: 1.2rem;
+        background: indigo;
+      }
+      header a{
+        color: darkgrey;
+        text-decoration: none;
+      }
+      header a:hover{
+        font-wight: bold;
+        color: lightgrey;
+      }
+      footer{
+        padding: 1em;
+      }
+
+    `}</style>
+    <style global jsx>{`
+      body{
+        magin: 0;
+        font-size: 110%;
+        background: #f0f0f0;
+      }
+    `}</style>
   </div>
 )
